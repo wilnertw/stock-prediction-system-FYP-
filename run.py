23347860db76b15f50fd894cc039c5f31d13ec17
wilnertw <wilnertw@gmail.com>
2023-10-09 22:30:58 +0800

    update 9.10(sign in module is finished)

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, StringField, PasswordField, validators
+from wtforms.validators import InputRequired
 from flask_bcrypt import Bcrypt
 from datetime import datetime
 import mysql.connector
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
 bcrypt = Bcrypt(app)
 
 #Database Connection
@@ -21,10 +25,31 @@ def view_homepage():
 @app.route('/login_page')
 def view_login(): 
     return render_template('loginPage.html')
+
+class registerForm(FlaskForm):
+    fullname = StringField('Full Name', [validators.InputRequired()])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Email(), validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
         
-@app.route('/register_page')
-def view_signin():
-    return render_template('registerPage.html') 
+@app.route('/register_page', methods=['POST', 'GET'])
+def signin():
+    form = registerForm()
+    if form.validate_on_submit():
+        fullname = form.fullname.data
+        username = form.username.data
+        email = form.email.data
+        psw = form.password.data
+        hashed_psw = bcrypt.generate_password_hash(psw).decode('utf-8')
+        register(fullname, username, email, hashed_psw)
+
+    return render_template('registerPage.html', form=form)
+
+    
 
 # @app.route('/login', methods = ['POST', 'GET'])
 # def login():
@@ -38,28 +63,15 @@ def view_signin():
 #         return f"YAY"
 #     else:
 #         return "Login via the login Form"
-    
 
-@app.route('/register', methods = ['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        
-        fullname = request.form['fullName']
-        username = request.form['username']
-        email = request.form['email']
-        psw = request.form['psw']
-        hashed_psw = bcrypt.generate_password_hash(psw).decode('utf-8')
-        cur.execute('''INSERT INTO users
+
+def register(fullname, username, email, hashed_psw):
+    cur.execute('''INSERT INTO users
                     (fullName, username, email, password, creationDate) 
                     VALUES(%s,%s,%s,%s,%s) ''', (fullname, username, email, hashed_psw, datetime.now()))
-        db.commit()
-        cur.close() 
-        return f"Sign In Successful"
-    else:
-        return f"Problem with entry"
-
-
-
+    db.commit()
+    cur.close()
+    
 
 if __name__ == "__main__" :
     app.run(debug=True, host='localhost', port=5000)
